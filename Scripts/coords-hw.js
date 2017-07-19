e36dffcba17a307f4e89384875a84b94334ebc6b
Greg Moten Jr <gmotenjr@gmail.com>
2017-07-19 15:47:08 -0400

    added HW for outside work, kata homework

---
+++ Scripts/coords-hw.js
@@ -0,0 +1,24 @@
+﻿function buttonCoordinates(xValue, yValue, array) {
+    array.push({ x: xValue, y: yValue });
+}
+// Create buttons array and add a few buttons
+var buttons = [];
+buttonCoordinates(0, 0, buttons);
+buttonCoordinates(0, 1, buttons);
+buttonCoordinates(2, 2, buttons);
+
+//to get X and Y values from array
+//buttons[0].x 
+//buttons[0].y 
+
+// to loop through coordinate values
+for (var i = 0; i < buttons.length; i++) {
+    var x = buttons[i].x;
+    var y = buttons[i].y;
+}
+
+buttons.sort();
+
+// still learning javascript I have spent the least time with but I am working though some tutorials and creating
+//some web apps on my GitHub 
+
