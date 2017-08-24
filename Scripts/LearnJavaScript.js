6174f65e2f7b63931039de572e29d4751659500b
Greg Moten Jr <gmotenjr@gmail.com>
2017-08-24 13:14:39 -0400

    Shopping List

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,14 +1,4 @@
 ﻿
-// Example
-var ourArray = ["Stimpson", "J", "cat"];
-ourArray.shift(); // ourArray now equals ["J", "cat"]
-ourArray.unshift("Happy");
-// ourArray now equals ["Happy", "J", "cat"]
+var myList = [["Choclate", 15], ["Fruit", 12], ["Ceral", 20], ["Ice Creame", 31], ["Pop", 16]];
 
-// Setup
-var myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-
-// Only change code below this line.
-myArray.unshift(["Paul", 35]);
 
