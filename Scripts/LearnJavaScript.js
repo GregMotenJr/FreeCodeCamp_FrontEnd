4e62cb771bec8243ab86fe0acc84f0d85845d410
Greg Moten Jr <gmotenjr@gmail.com>
2017-08-27 20:07:44 +0000

    Understanding Boolean Values

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,15 +1,9 @@
 
-function nextInLine(arr, item) {
-  arr.push(item);
-  var answer=arr.shift();
-  
-  return answer;  // Change this line
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return false; // Change this line
+
+// Only change code above this line.
 }
-
-// Test Setup
-var testArr = [1,2,3,4,5];
-
-// Display Code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6)); // Modify this line to test
-console.log("After: " + JSON.stringify(testArr));
