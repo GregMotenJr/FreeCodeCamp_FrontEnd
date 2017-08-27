381c7577a194ba45f3a143f0a4e734f607223ad5
Greg Moten Jr <gmotenjr@gmail.com>
2017-08-27 03:59:46 +0000

    Stand in Line

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,20 +1,15 @@
 
-// Example
-var changed = 0;
-
-function change(num) {
-  return (num + 5) / 3;
+function nextInLine(arr, item) {
+  arr.push(item);
+  var answer=arr.shift();
+  
+  return answer;  // Change this line
 }
 
-changed = change(10);
+// Test Setup
+var testArr = [1,2,3,4,5];
 
-// Setup
-var processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
-}
-
-// Only change code below this line
-
-processed = processArg(7);
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
