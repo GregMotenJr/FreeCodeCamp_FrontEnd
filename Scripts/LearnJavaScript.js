633df9a32830cf8f01504fad0726d53aceefd6a2
Greg Moten Jr <gmotenjr@gmail.com>
2017-08-29 22:26:57 -0400

    Comparison with the Inequality Operator

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,24 +1,11 @@
-
-// Example
-function ourTrueOrFalse(isItTrue) {
-  if (isItTrue) { 
-    return "Yes, it's true";
-  }
-  return "No, it's false";
-}
-
+﻿
 // Setup
-function trueOrFalse(wasThatTrue) {
-
-  // Only change code below this line.
-  if (wasThatTrue){
-    return "Yes, that was true";
-  }
-  return "No, that was false";
-  
-  // Only change code above this line.
-
+function testNotEqual(val) {
+    if (val != 99) {
+        return "Not Equal";
+    }
+    return "Equal";
 }
 
 // Change this value to test
-trueOrFalse(true);
+testNotEqual(10);
