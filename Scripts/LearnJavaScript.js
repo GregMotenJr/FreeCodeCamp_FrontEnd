e3b497bbde1aab500703e064f338113a3483185c
Greg Moten Jr <gmotenjr@gmail.com>
2017-08-31 08:44:30 -0400

    Comparisons with the Logical Or Operator

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,14 +1,14 @@
 ﻿
-function testLogicalAnd(val) {
+function testLogicalOr(val) {
     // Only change code below this line
 
-    if (val <= 50 && val >= 25) {
-        return "Yes";
+    if (val <= 9 || val > 20) {
+        return "Outside";
     }
 
     // Only change code above this line
-    return "No";
+    return "Inside";
 }
 
 // Change this value to test
-testLogicalAnd(10);
+testLogicalOr(10);
