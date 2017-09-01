43a0a0dcfddcda73adee61dd921c547adbbfcf98
Greg Moten Jr <gmotenjr@gmail.com>
2017-09-01 15:47:32 -0400

    Selecting from many options with Switch Statements

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,28 +1,30 @@
 ﻿
-function golfScore(par, strokes) {
+function caseInSwitch(val) {
+    var answer = "";
     // Only change code below this line
-    if (strokes == 1) {
-        return "Hole-in-one!";
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+
+        case 2:
+            answer = "beta";
+            break;
+
+        case 3:
+            answer = "gamma";
+            break;
+
+        case 4:
+            answer = "delta";
+            break;
     }
-    else if (strokes <= par - 2) {
-        return "Eagle";
-    }
-    else if (strokes == par - 1) {
-        return "Birdie";
-    }
-    else if (strokes == par) {
-        return "Par";
-    }
-    else if (strokes == par + 1) {
-        return "Bogey";
-    }
-    else if (strokes == par + 2) {
-        return "Double Bogey";
-    }
-    else
-        return "Go Home!";
-    // Only change code above this line
+
+
+    // Only change code above this line  
+    return answer;
 }
 
-// Change these values to test
-golfScore(4, 5);
+// Change this value to test
+caseInSwitch(1);
+
