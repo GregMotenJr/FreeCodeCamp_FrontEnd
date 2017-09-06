271827ef9e75ca010d57e67e575d1d7fed435d52
Greg Moten Jr <gmotenjr@gmail.com>
2017-09-05 22:41:17 -0400

    Counting Cards

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,15 +1,46 @@
 
-// Setup
-function abTest(a, b) {
-    // Only change code below this line
-    if (a <= 0 || b <= 0) {
-        return undefined;
-    }
-    else
-        // Only change code above this line
+var count = 0;
 
-        return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+function cc(card) {
+    // Only change code below this line
+    var bet;
+
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+
+        case 7:
+        case 8:
+        case 9:
+            count = count;
+            break;
+
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            count--;
+            break;
+
+    }
+
+
+    if (count >= 1)
+        bet = " Bet";
+    if (count <= 0)
+        bet = " Hold";
+
+    return count + bet;
+
+    // Only change code above this line
 }
 
-// Change values below to test your code
-abTest(2, 2);
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
