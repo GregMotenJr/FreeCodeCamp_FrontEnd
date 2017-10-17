4dc1897377b8e66ddcc65cf9a77bcbb4552d79cc
GregMotenJr <gmotenjr@gmail.com>
2017-10-16 22:39:07 -0400

    Iterate with JavaScript For Loops

--- Scripts/LearnJavaScript.js
+++ Scripts/LearnJavaScript.js
@@ -1,25 +1,17 @@
 
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+    ourArray.push(i);
+}
+
 // Setup
-var myPlants = [
-    {
-        type: "flowers",
-        list: [
-            "rose",
-            "tulip",
-            "dandelion"
-        ]
-    },
-    {
-        type: "trees",
-        list: [
-            "fir",
-            "pine",
-            "birch"
-        ]
-    }
-];
+var myArray = [];
 
-// Only change code below this line
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
 
-var secondTree = myPlants[1].list[1]; // Change this line
 
